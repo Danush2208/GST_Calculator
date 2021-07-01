b576ae9a83be1c90d5f66124fa43f215081511dd
S DANUSH VENKATESH <danushvenkat25@gmail.com>
2021-07-01 14:10:31 +0530

    final_code_change

--- 3_Implementation/start.c
+++ 3_Implementation/start.c
@@ -12,14 +12,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <conio.h>
 #include "fun.h"
 
 int main(){
   char pdt[20],lic[20];
-  int option,gst_percent,gst_charged,license_check;
-  system("CLS");
+  int option,gst_percent,gst_charged,license_check,percent_check;
+NEW:system("CLS");
   printf("\n\t\t\t\t 1.DISPLAY YOUR PRODUCT'S GST");
-  printf("\n\t\t\t\t 2.CHCK GST OF YOUR PRODUCT");
+  printf("\n\t\t\t\t 2.CHECK GST OF YOUR PRODUCT");
   printf("\n\t\t\t\t 3.GST LISCENCE CHECKER");
   printf("\n\t\t\t\t 4.EXIT");
   printf("\n\n\n\t\t CHOOSE ANYONE OPTION(1-4):");
@@ -49,9 +50,12 @@ int main(){
     if(gst_percent==28){
       printf("%d percentage",gst_percent);
     }
+    printf("%c",getch());
+    goto NEW;
   }
   if(option==2){
     system("CLS");
+    percent_check=0;
     printf("Enter the product you want to know the GST for:");
     scanf("%s",pdt);
     printf("Enter the GST charged for your product:");
@@ -59,24 +63,48 @@ int main(){
     gst_percent=product_zero(pdt);
     if(gst_percent==0 && gst_charged==0){
       printf("You have been charged with the rite percentage of GST");
+      percent_check=0;
+    }
+    else{
+      percent_check=1;
     }
     gst_percent=product_five(pdt);
     if(gst_percent==5 && gst_charged==5){
       printf("You have been charged with the rite percentage of GST");
+      percent_check=0;
+    }
+    else{
+      percent_check=1;
     }
     gst_percent=product_twelve(pdt);
     if(gst_percent==12 && gst_charged==12){
       printf("You have been charged with the rite percentage of GST");
+      percent_check=0;
+    }
+    else{
+      percent_check=1;
     }
     gst_percent=product_eighteen(pdt);
     if(gst_percent==18 && gst_charged==18){
       printf("You have been charged with the rite percentage of GST");
+      percent_check=0;
+    }
+    else{
+      percent_check=1;
     }
     gst_percent=product_twentyeight(pdt);
     if(gst_percent==28 && gst_charged==28){
       printf("You have been charged with the rite percentage of GST");
+      percent_check=0;
+    } 
+    else{
+      percent_check=1;
     }
-    printf("You have been chagred inncorrect GST"); 
+    if(percent_check==1){
+      printf("You have been charged inccorect GST");
+    }
+    printf("%c",getch());
+    goto NEW;
   }
   if(option==3){
     system("CLS");
@@ -86,7 +114,10 @@ int main(){
     if(license_check==0){
       printf("The GST number is perfect ");
     }
+    printf("%c",getch());
+    goto NEW;
   }
+  if(option==4)
   system("CLS");
   return 0;
 }
