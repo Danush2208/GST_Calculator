15ed783507ebba96f16836a7a2071d9f9eb04e58
S DANUSH VENKATESH <danushvenkat25@gmail.com>
2021-07-12 13:53:36 +0530

    test file

--- 3_Implementation/test/test_calculator_operations.c
+++ 3_Implementation/test/test_calculator_operations.c
@@ -2,11 +2,11 @@
 #include "unity.h"
 
 //void test_zero(void);
-void test_five(void);
-/*void test_twelve(void);
-void test_eighteen(void);
-void test_twentyeight(void);
-void test_license(void);*/
+//void test_five(void);
+void test_twelve(void);
+//void test_eighteen(void);
+//void test_twentyeight(void);
+//void test_license(void);
 
 void setUp(){}
 void tearDown(){}
@@ -16,11 +16,11 @@ int main()
   UNITY_BEGIN();
 
   //RUN_TEST(test_zero);
-  RUN_TEST(test_five);
-  /*RUN_TEST(test_twelve);
-  RUN_TEST(test_eighteen);
-  RUN_TEST(test_twentyeight);
-  RUN_TEST(test_license);*/
+  //RUN_TEST(test_five);
+  RUN_TEST(test_twelve);
+  //RUN_TEST(test_eighteen);
+  //RUN_TEST(test_twentyeight);
+  //RUN_TEST(test_license);*/
 
   return UNITY_END();
 }
@@ -29,20 +29,20 @@ int main()
 {
   char test_case_1[5]="milk";
   TEST_ASSERT_EQUAL(0, product_zero(test_case_1));
-}*/
+}
 
 void test_five(void) 
 {
   char test_case_2[7]="cloves";
   TEST_ASSERT_EQUAL(5, product_five(test_case_2));
 }
-/*
+*/
 void test_twelve(void) 
 {
   char test_case_3[7]="butter";
   TEST_ASSERT_EQUAL(12, product_twelve(test_case_3));
 }
-
+/*
 void test_eighteen(void) {
   char test_case_4[8]="watches";
   TEST_ASSERT_EQUAL(18, product_eighteen(test_case_4));
