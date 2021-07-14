04bf9a6ed36bbf86b42d1774c2835de0f85dc7d6
S DANUSH VENKATESH <danushvenkat25@gmail.com>
2021-07-14 19:23:30 +0530

    changes

--- 3_Implementation/src/zero.c
+++ 3_Implementation/src/zero.c
@@ -12,14 +12,16 @@ int product_zero(char pdt[]){
   FILE *fp=NULL;
     fp=fopen("../3_Implementation/text_files/zero.txt","r");
     if(fp){ 
+      char* dum_pdt;
     while(!feof(fp))
         {
           int len;
           len=strlen(pdt)+1;
-          char* dum_pdt;
+          
           dum_pdt = (char*)malloc(len * sizeof (char));
           fgets(dum_pdt,len,fp);
            if(strcmp(dum_pdt,pdt)==0){
+             dum_pdt=NULL;
                return 0;
            }
         }
